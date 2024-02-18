c3f955f768d925706e3f480147f0f50cd733b113
Lucie Yarish <lucieyarish@hotmail.com>
2024-02-18 19:06:34 +0100

    Create Info component - WIP

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Info from './containers/Info';
+
+const App = () => {
+  return (
+    <div className="card-container">
+      <Info />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/containers/Info.jsx
@@ -0,0 +1,22 @@
+import './Info.css';
+import Link from '../components/Link';
+
+const Info = () => {
+  return (
+    <header className="header">
+      <img
+        src="/src/assets/images/lucie-profile-photo.jpg"
+        alt="Profile photo of Lucie Yarish"
+        className="header--profile-photo"
+      />
+      <h1 className="header--title">Lucie Yarish</h1>
+      <h2 className="header--subtitle">Software Developer</h2>
+      <div className="header--links__container">
+        <Link text={'Email'} type={'email'} />
+        <Link text={'LinkedIn'} type={'linkedin'} />
+      </div>
+    </header>
+  );
+};
+
+export default Info;
