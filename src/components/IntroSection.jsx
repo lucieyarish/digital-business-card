b75456a6cf28b2e5e87bfe1e7f519c3edf62ac7b
Lucie Yarish <lucieyarish@hotmail.com>
2024-02-21 07:11:09 +0100

    Increase padding; fix typo; add box shadow

--- src/components/IntroSection.jsx
+++ src/components/IntroSection.jsx
@@ -2,7 +2,7 @@ import './IntroSection.css';
 
 const IntroSection = (props) => {
   return (
-    <article class="intro-section">
+    <article className="intro-section">
       <h3 className="intro-section--title">{props.title}</h3>
       <p className="intro-section--content">{props.content}</p>
     </article>
