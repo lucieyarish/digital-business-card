4111965d3b5cc64f15ce695e4a35b85ce8d3601f
Lucie Yarish <lucieyarish@hotmail.com>
2024-02-21 20:38:51 +0100

    Fix typo

--- src/containers/Intro.jsx
+++ src/containers/Intro.jsx
@@ -4,8 +4,8 @@ import IntroSection from '../components/IntroSection';
 const Intro = () => {
   const aboutSectionTitle = 'About';
   const interestsSectionTitle = 'Interests';
-  const aboutSectionContent = `I am a software developer with a particular interest in frontend development and web accessibility. I love building resposive and interactive websites, and connecting with like-minded people, both within and outside the tech industry. I often talk about my learnings and progress on Instagram and LinkedIn.`;
-  const interestsSectionContent = `When I'm not coding, I enjoy moving my body, reading, cooking & baking, drawing, listening to music & audio books, and going for walks. As a former sinologist, I also enjoy keeping up with my knowledge of Mandarin.`;
+  const aboutSectionContent = `I am a software developer with a particular interest in frontend development and web accessibility. I love building responsive and interactive websites, and connecting with like-minded people, both within and outside the tech industry. I often talk about my learnings and progress on Instagram and LinkedIn.`;
+  const interestsSectionContent = `When I'm not coding, I enjoy moving my body, reading, cooking & baking, drawing, listening to music & audio books, and going for walks. As a former sinologist, I also enjoy keeping up with my knowledge of Chinese.`;
 
   return (
     <main className="main-container">
