5c1479d046daf5a5125565332e9d44b0eb1efe29
Lucie Yarish <lucieyarish@hotmail.com>
2024-02-21 07:10:32 +0100

    Remove commented code; fix typo; fix class -> className

--- src/containers/Intro.jsx
+++ src/containers/Intro.jsx
@@ -4,11 +4,11 @@ import IntroSection from '../components/IntroSection';
 const Intro = () => {
   const aboutSectionTitle = 'About';
   const interestsSectionTitle = 'Interests';
-  const aboutSectionContent = `I am a software developer with a particular interest in frontend development and web accessibility. I love building resposive and interactive websites, and connecting with like-minded people, both within and outside the tech industry. I often talk about my learnings and progress on Instagram and Linkedin.`;
+  const aboutSectionContent = `I am a software developer with a particular interest in frontend development and web accessibility. I love building resposive and interactive websites, and connecting with like-minded people, both within and outside the tech industry. I often talk about my learnings and progress on Instagram and LinkedIn.`;
   const interestsSectionContent = `When I'm not coding, I enjoy moving my body, reading, cooking & baking, drawing, listening to music & audio books, and going for walks. As a former sinologist, I also enjoy keeping up with my knowledge of Mandarin.`;
 
   return (
-    <main>
+    <main className="main-container">
       <IntroSection title={aboutSectionTitle} content={aboutSectionContent} />
       <IntroSection
         title={interestsSectionTitle}
