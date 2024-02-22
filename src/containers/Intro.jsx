9c5553cec38fce7d442627ef92011d3b749cc58e
Lucie Yarish <lucieyarish@hotmail.com>
2024-02-22 06:12:29 +0100

    Rewrite intro

--- src/containers/Intro.jsx
+++ src/containers/Intro.jsx
@@ -4,8 +4,8 @@ import IntroSection from '../components/IntroSection';
 const Intro = () => {
   const aboutSectionTitle = 'About';
   const interestsSectionTitle = 'Interests';
-  const aboutSectionContent = `I am a software developer with a particular interest in frontend development and web accessibility. I love building responsive and interactive websites, and connecting with like-minded people, both within and outside the tech industry. I often talk about my learnings and progress on Instagram and LinkedIn.`;
-  const interestsSectionContent = `When I'm not coding, I enjoy moving my body, reading, cooking & baking, drawing, listening to music & audio books, and going for walks. As a former sinologist, I also enjoy keeping up with my knowledge of Chinese.`;
+  const aboutSectionContent = `Lucie is a software developer with a particular focus on frontend development and web accessibility. She enjoys creating responsive and interactive websites, and connecting with like-minded people, both within and outside the tech industry. She frequently shares her learnings and progress on platforms such as Instagram and LinkedIn.`;
+  const interestsSectionContent = `When she's not coding, she enjoys engaging in physical activities, reading, cooking & baking, drawing, listening to music & audio books, and going for walks. Lucie also finds joy in embracing minimalist living, both in the physical and digital world. As a former sinologist, she occasionally likes to keep up with her knowledge of Chinese.`;
 
   return (
     <main className="main-container">
